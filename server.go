24cea6c06da7bc18791b646787f78c8a9800335e
Ming-der Wang <ming@log4analytics.com>
2015-02-10 18:24:37 +0800

    added users database for login and sessions

--- server.go
+++ server.go
@@ -6,9 +6,15 @@ import (
 	"github.com/go-martini/martini"
 	_ "github.com/lib/pq"
 	"github.com/martini-contrib/logstasher"
+	"github.com/martini-contrib/sessions"
 	"github.com/mipearson/rfw"
 	"net/http"
 )
+
+type User struct {
+	Name  string
+	Email string
+}
 
 func SetupDB() *sql.DB {
 	// change user=XXX to your Postgres database user name.
@@ -35,8 +41,12 @@ func main() {
 	m.Use(logstasher.Logger(logstashLogFile))
 	m.Use(martini.Static("public"))
 
-	// get
-	m.Get("/book", func(db *sql.DB, r *http.Request, rw http.ResponseWriter) {
+	// Sessions
+	store := sessions.NewCookieStore([]byte("12353453123123123"))
+	m.Use(sessions.Sessions("goes_auth", store))
+
+	m.Get("/hello", RequireLogin, SecretPath)
+	m.Get("/book", RequireLogin, func(db *sql.DB, r *http.Request, rw http.ResponseWriter) {
 		search := "%" + r.URL.Query().Get("search") + "%"
 		rows, err := db.Query(`SELECT title, author, description
                 FROM books
@@ -54,19 +64,42 @@ func main() {
 		}
 	})
 
-	// login
 	m.Post("/login", PostLogin)
+	m.Get("/logout", Logout)
 	m.Run()
 }
 
-func PostLogin(req *http.Request, db *sql.DB) (int, string) {
+func SecretPath(user *User) string {
+	return "Great Job " + user.Name
+}
+
+func PostLogin(req *http.Request, db *sql.DB, s sessions.Session) (int, string) {
 	var id string
 
 	email, password := req.FormValue("email"), req.FormValue("password")
 	err := db.QueryRow("select id from users where email=$1 and password=$2", email, password).Scan(&id)
 
 	if err != nil {
-		return 401, "Unauthorized"
+		return http.StatusUnauthorized, "Unauthorized"
 	}
-	return 200, "User id is " + id
+
+	s.Set("userId", id)
+	return http.StatusOK, "User id is " + id
+}
+
+func RequireLogin(rw http.ResponseWriter, req *http.Request,
+	s sessions.Session, db *sql.DB, c martini.Context) {
+	user := &User{}
+	err := db.QueryRow("select name, email from users where id=$1", s.Get("userid")).Scan(&user.Name, &user.Email)
+
+	if err != nil {
+		http.Redirect(rw, req, "/login", http.StatusFound)
+		return
+	}
+	c.Map(user)
+}
+
+func Logout(rw http.ResponseWriter, req *http.Request, s sessions.Session) {
+	s.Delete("userId")
+	http.Redirect(rw, req, "/", http.StatusFound)
 }
---
+++ users.sql
@@ -0,0 +1,6 @@
+CREATE TABLE users (
+    id integer NOT NULL,
+    name character varying(70) NOT NULL,
+    email character varying(254) NOT NULL,
+    password character varying(128) NOT NULL,
+);
